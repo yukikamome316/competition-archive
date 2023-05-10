753507b757d12d03d74c63d96d4e955e6ca2b16b
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-05-10 10:19:32 +0900

    fix: res/template/*.cppのfor文スニペットを変更

--- res/template/ALC_template.cpp
+++ res/template/ALC_template.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #include <atcoder/all>
-#define rep(i, n) for (int i = 0; i < (int)n; i++)
-#define rep2(i, s, n) for (int i = (int)s; i < (int)n; i++)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
 using namespace std;
 using namespace atcoder;
 using ll = long long;
