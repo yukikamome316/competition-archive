8dc6e0023a2a303751f70ab988b4bdece77cb080
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-06-02 10:56:42 +0900

    docs: ABC121D

---
+++ src/ABC/ABC121/D.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+ll calcXorTrick(ll n) {
+    if (n % 4 == 0) return n;
+    if (n % 4 == 1) return 1;
+    if (n % 4 == 2) return n + 1;
+    return 0;
+}
+
+int main() {
+    ll A, B;
+    cin >> A >> B;
+
+    cout << (calcXorTrick(B) ^ calcXorTrick(A - 1)) << endl;
+}
