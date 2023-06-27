61761ad93032015b531f858d821e43259ceb2911
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-06-27 09:29:32 +0900

    docs: ABC048B

---
+++ src/ABC/ABC048/B.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  ll a, b, x;
+  cin >> a >> b >> x;
+  cout << b / x - a / x + (a % x == 0) << endl;
+}
