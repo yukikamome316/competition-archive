051cd6fda1ae1671532a0530f2fb575f08bd9f0c
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-09-28 14:35:06 +0900

    docs: ABC321C

--- src/ABC/ABC321/C.cpp
+++ src/ABC/ABC321/C.cpp
@@ -6,37 +6,24 @@ using ll = long long;
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
 
-bool isLikeNum(ll x) {
-  string N = to_string(x);
-  if (N.size() > 10) {
-    return false;
-  }
-
-  bool ok = true;
-  rep(i, N.size() - 1) {
-    if (N[i] <= N[i + 1]) ok = false;
-  }
-
-  return ok;
-}
-
 int main() {
   int K;
   cin >> K;
 
-  string nums = "1234567890";
+  vector<ll> nums;
 
-  vector<ll> arr;
-  for (ll bit = 1; bit < (1ll << 10); bit++) {
-    string tmp = "";
-    rep(i, 10) {
-      if (bit & (1ll << i)) {
-        tmp +=  nums[i];
+  rep2(bits, 1, 1ll << 10) {
+    ll num = 0;
+    for (int i = 9; i >= 0; i--) {
+      if (bits & (1ll << i)) {
+        num *= 10;
+        num += i;
       }
     }
-    arr.push_back(stoll(tmp));
+
+    nums.push_back(num);
   }
 
-  sort(arr.begin(), arr.end());
-  cout << arr[K - 1] << endl;
+  sort(nums.begin(), nums.end());
+  cout << nums[K] << endl;
 }
