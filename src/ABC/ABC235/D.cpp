f75deb134cc6593a2de11ae7139e41c31136c877
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-10-31 17:18:27 +0900

    feat: ABC235D AC

--- src/ABC/ABC235/D.cpp
+++ src/ABC/ABC235/D.cpp
@@ -6,8 +6,6 @@ using ll = long long;
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
 
-int a, N;
-
 int getDigit(ll num) {
   int ret = 0;
   while (num != 0) {
@@ -17,43 +15,42 @@ int getDigit(ll num) {
   return ret;
 }
 
-int dfs(ll x, int N_len, int cnt, bool swapped, set<ll> &seen) {
-  if (x == N) return cnt;
-  
-  if (seen.count(x)) {
-    cout << x << " duplication halt." << endl;
-    return -1;
-  }
-
-  seen.insert(x);
-  
-  int x_len = getDigit(x);
-  if (x_len > N_len) {
-    cout << x << " halt." << endl;
-    return -1;
-  }
-
-  int ret = -1;
-  if (!swapped && x >= 10 && x % 10 != 0) {
-    string x_str = to_string(x);
-    x_str.insert(x_str.begin(), x_str[x_len - 1]);
-    x_str.erase(x_str.begin() + x_len);
-    
-    ll x_swapped = stoll(x_str);
-    cout << x << "->" << x_swapped << " (insert)" << endl;
-    ret = dfs(x_swapped, N_len, cnt + 1, true, seen);
-  }
-
-  if (ret != -1) return ret;
-
-  cout << x << "->" << x * a << endl;
-  return dfs(x * a, N_len, cnt + 1, false, seen);
-}
 
 int main() {
+  int a, N;
   cin >> a >> N;
-  int N_len = getDigit(N);
 
+  int N_len = getDigit(N);
+  
   set<ll> seen;
-  cout << dfs(1, N_len, 0, false, seen) << endl;
+  queue<pair<ll, int>> que;
+  que.push({1, 0});
+
+  while (!que.empty()) {
+    auto [x, cnt] = que.front();
+    que.pop();
+
+    if (seen.count(x)) continue;
+    seen.insert(x);
+
+    if (x == N) {
+      cout << cnt << endl;
+      return 0;
+    }
+
+    int x_len = getDigit(x);
+    if (x_len > N_len) continue;
+
+    if (x >= 10 && x % 10 != 0) {
+      string x_str = to_string(x);
+      
+      x_str.insert(x_str.begin(), x_str[x_len - 1]);
+      x_str.erase(x_str.begin() + x_len);
+      que.push({stoll(x_str), cnt + 1});
+    }
+    
+    que.push({x * a, cnt + 1});
+  }
+
+  cout << -1 << endl;
 }
