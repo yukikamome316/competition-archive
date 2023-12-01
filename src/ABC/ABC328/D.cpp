4ac2d821db12569e7963166b5f14a48e7a30626e
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-12-01 17:15:20 +0900

    feat: ABC328D

--- src/ABC/ABC328/D.cpp
+++ src/ABC/ABC328/D.cpp
@@ -9,6 +9,30 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; }
 int main() {
   string S;
   cin >> S;
-  vector<int> next(S.size());
+
+  stack<char> st;
+  rep(i, S.size()) {
+    st.push(S[i]);
+    if (st.size() >= 3) {
+      char c = st.top();  st.pop();
+      char b = st.top();  st.pop();
+      char a = st.top();  st.pop();
+      if (a == 'A' && b == 'B' && c == 'C') {
+        continue;
+      } else {
+        st.push(a);
+        st.push(b);
+        st.push(c);
+      }
+    }
+  }
   
+  string ans = "";
+  while (!st.empty()) {
+    ans += st.top();
+    st.pop();
+  }
+
+  reverse(ans.begin(), ans.end());
+  cout << ans << endl;
 }
